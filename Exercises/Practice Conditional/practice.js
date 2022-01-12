1c397892be78a11d386a3c3f94dc42d7519d539f
Bernardo <bernardo_pprz@comunidad.unam.mx>
2022-01-12 16:31:20 -0600

    Finished Practice Condition

--- Exercises/Practice Conditional/practice.js
+++ Exercises/Practice Conditional/practice.js
@@ -20,5 +20,34 @@ function dayCalculate(dayNumber) {
   }
 }
 
+function otherDayCalculate(dayNumber) {
+  switch (dayNumber) {
+    case 0:
+      return "Domingo";
+      break;
+    case 1:
+      return "Lunes";
+      break;
+    case 2:
+      return "Martes";
+      break;
+    case 3:
+      return "Miercoles";
+      break;
+    case 4:
+      return "Jueves";
+      break;
+    case 5:
+      return "Viernes";
+      break;
+    case 6:
+      return "Sabado";
+      break;
+    default:
+      return "Número de dia invalido";
+      break;
+  }
+}
+
 let dayNumber = Number(prompt("Introduce el numero del dia (0-6):"));
-console.log(dayCalculate(dayNumber));
+console.log(otherDayCalculate(dayNumber));
